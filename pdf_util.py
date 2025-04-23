dbc7d27d71f038cafb6485c1bb65831b7ac0db1c
No-one9 <chnlokesh9@gmail.com>
2025-04-22 23:41:47 -0500

    latest push

--- pdf_util.py
+++ pdf_util.py
@@ -1,14 +1,14 @@
-import pdfplumber
-def extract_pdf_metadata(pdf_path):
-    with pdfplumber.open(pdf_path) as pdf:
-        return{
-            "author":pdf.metadata.get("Author",""),
-            "title": pdf.metadata.get("Title",""),
-            "pages": len(pdf.pages)
-        }
-def extract_pdf_tables(pdf_path):
-    tables=[]
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            tables.extend(page.extract_tables())
-    return tables
+# import pdfplumber
+# def extract_pdf_metadata(pdf_path):
+#     with pdfplumber.open(pdf_path) as pdf:
+#         return{
+#             "author":pdf.metadata.get("Author",""),
+#             "title": pdf.metadata.get("Title",""),
+#             "pages": len(pdf.pages)
+#         }
+# def extract_pdf_tables(pdf_path):
+#     tables=[]
+#     with pdfplumber.open(pdf_path) as pdf:
+#         for page in pdf.pages:
+#             tables.extend(page.extract_tables())
+#     return tables
